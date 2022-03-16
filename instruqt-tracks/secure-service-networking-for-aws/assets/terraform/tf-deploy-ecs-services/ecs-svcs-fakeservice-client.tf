6e3d99bc84e01f83513591856f55303d9afc6613
Nathan Pearce <nathan.pearce@gmail.com>
2022-03-16 15:07:36 -0700

    dupe

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-ecs-services/ecs-svcs-fakeservice-client.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-ecs-services/ecs-svcs-fakeservice-client.tf
@@ -1,15 +1,4 @@
 locals {
-  example_server_app_log_config = {
-    logDriver = "awslogs"
-    options = {
-      awslogs-group         = aws_cloudwatch_log_group.log_group.name
-      awslogs-region        = var.region
-      awslogs-stream-prefix = "app"
-    }
-  }
-}
-
-locals {
   example_client_app_log_config = {
     logDriver = "awslogs"
     options = {
